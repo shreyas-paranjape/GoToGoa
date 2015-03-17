651d038fa8923fcb0982e3821a60da6101993c50
Shraddha naik <naik_shraddha@rocketmail.com>
2015-03-17 15:59:24 +0530

    second commit

--- presentation/app/module/hotel/script/controller.js
+++ presentation/app/module/hotel/script/controller.js
@@ -1,14 +1,45 @@
 angular.module('hotel')
 
-.controller('HotelsController', ['$scope', '$stateParams', function ($scope, $stateParams, $stateProvider, $urlRouterProvider) {
+.controller('HotelsController', ['$scope', '$filter', '$stateParams', function ($scope, $filter, $stateParams, $stateProvider, $urlRouterProvider) {
     //TODO Here we use the data passed from the form to lookup the hotels
     
     $scope.items = $stateParams.data;
     
     $scope.hotels = [
-    {"id":1, "name":"Vivanta", "location":"A", "stars":5},
-    {"id":2, "name":"Ramada", "location":"B", "stars":4},
-    {"id":3, "name":"Novotel", "location":"A", "stars":3},
-    {"id":4, "name":"Radisson", "location":"B", "stars":2}
-];
+    {"id":1, "name":"Vivanta", "location":"A", "stars":2},
+    {"id":2, "name":"Ramada", "location":"B", "stars":3},
+    {"id":3, "name":"Novotel", "location":"A", "stars":4},
+    {"id":4, "name":"Radisson", "location":"B", "stars":5}
+    ];
+    
+    var orderBy = $filter('orderBy');
+    
+    $scope.order = function(predicate, reverse) {
+      $scope.hotels = orderBy($scope.hotels, predicate, reverse);
+    };
+    
+    
+    $scope.locationIncludes = [];
+    
+    $scope.includeLocation = function(location) {
+        var i = $.inArray(location, $scope.locationIncludes);
+        if (i > -1) {
+            $scope.locationIncludes.splice(i, 1);
+        } else {
+            $scope.locationIncludes.push(location);
+        }
+    }
+    
+    $scope.locationFilter = function(hotels) {
+        if ($scope.locationIncludes.length > 0) {
+            if ($.inArray(hotels.location, $scope.locationIncludes) < 0)
+                return;
+        }
+        
+        return hotels;
+    }
+
 }]);
+
+
+  
