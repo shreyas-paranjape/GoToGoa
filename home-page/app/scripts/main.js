2542455b502d00d9d8f89810a409d123cc937dfe
Shreyas Paranjape <sparanjape89@gmail.com>
2015-06-01 09:31:39 +0530

    changed the api doc

--- home-page/app/scripts/main.js
+++ home-page/app/scripts/main.js
@@ -2,13 +2,15 @@
 $( document ).ready(function() {
     $('.jumbo-wrapper').height($(window).height());
     $('.gridster .grid').gridster({
-        'widget_margins': [0,0],
-        'widget_base_dimensions': [200, 200],
+        'widget_margins': [10,10],
+        'widget_base_dimensions': [190, 125],
         'widget_selector': 'section'
-    });
-    $('.gridster .grid section').each(function() {
+    }).data('gridster');
+   
+    /* add  randome color to grid sections */
+     $('.gridster .grid section').each(function() {
         var hue = 'rgb(' + (Math.floor((256-199)*Math.random()) + 200) + ',' + 
 		(Math.floor((256-100)*Math.random()) + 200) + ',' + (Math.floor((256-220)*Math.random()) + 200) + ')';
-         $(this).css('background-color', hue);
+         $(this).css({'background-color': hue});
     });
 });
