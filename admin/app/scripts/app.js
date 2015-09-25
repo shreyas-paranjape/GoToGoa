f44e311abddb0e4092f62f9822bd55634233841f
Shreyas Paranjape <sparanjape89@gmail.com>
2015-09-25 09:54:34 +0530

    modified deals module

--- admin/app/scripts/app.js
+++ admin/app/scripts/app.js
@@ -1,55 +1,46 @@
 /*global angular:false */
-angular.element(document).ready(function () {
-    'use strict';
-    /**
-     * @ngdoc overview
-     * @name todoApp
-     * @description
-     * # todoApp
-     *
-     * Main module of the application.
-     */
-    angular
-        .module('gotogoa', [
-            'ngAnimate',
-            'ngCookies',
-            'restangular',
-            'ui.router',
-            'ncy-angular-breadcrumb',
-            'permission',
-            'ngSanitize',
-            'ngTouch',
-            'ui.bootstrap',
-            'ui.grid',
-            'trip',
-            'vendors',
-            'place',
-            'stay',
-            'travel',
-//            'ngMap',
-            'ui.calendar',
-            'ui.grid.edit',
-            'ui.grid.selection',
-            'angularModalService',
-            'schemaForm',
-            'mgcrea.ngStrap',
-            'schemaForm-datepicker',
-            'schemaForm-timepicker',
-            'schemaForm-datetimepicker',
-            'vendors',
-            'deals'
+angular.element(document).ready(function() {
+  'use strict';
+  angular
+    .module('gotogoa', [
+      'ngAnimate',
+      'ngCookies',
+      'restangular',
+      'ui.router',
+      'ncy-angular-breadcrumb',
+      'permission',
+      'ngSanitize',
+      'ngTouch',
+      'ui.bootstrap',
+      'ui.grid',
+      'trip',
+      'vendors',
+      'place',
+      'stay',
+      'travel',
+      'ui.calendar',
+      'ui.grid.edit',
+      'ui.grid.selection',
+      'angularModalService',
+      'schemaForm',
+      'mgcrea.ngStrap',
+      'schemaForm-datepicker',
+      'schemaForm-timepicker',
+      'schemaForm-datetimepicker',
+      'vendors',
+      'deals'
 
-        ])
-        .config(function ($breadcrumbProvider) {
-            $breadcrumbProvider.setOptions({
-                prefixStateName: 'vendors.list',
-                template: 'bootstrap2'
-            });
-        })
-        .config(function ($urlRouterProvider) {
-            $urlRouterProvider.otherwise('/place.list');
+    ])
+    .config(function($breadcrumbProvider) {
+      $breadcrumbProvider.setOptions({
+        prefixStateName: 'vendors.list',
+        template: 'bootstrap2'
+      });
+    })
+    .config(function($urlRouterProvider) {
+      $urlRouterProvider.otherwise('/deals');
 
-        });
+    });
 
-    angular.bootstrap(document, ['gotogoa']);
+  angular.bootstrap(document, ['gotogoa']);
 });
