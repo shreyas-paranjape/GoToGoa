775db7bf8e5a1e644534f685329bfea63b0361d4
Ruchita <ruchitaelekar05@gmail.com>
2016-01-22 18:06:42 +0530

    new changes made

--- presentation/app/directive/preferences/base.js
+++ presentation/app/directive/preferences/base.js
@@ -85,134 +85,49 @@ angular.module('directives')
             }
   }
 ])
-    .controller('PaceCardController', ['$scope', 'Restangular', '$attrs', '$state',
+
+.controller('CardController', ['$scope', 'Restangular', '$attrs', '$state',
   function ($scope, Restangular, $attrs, $state) {
-            this.init = function (element) {};
-            $scope.states = ['event', 'category'];
-            $scope.currentState = $scope.states[0];
-            //        $scope.activity = [];
+        this.init = function (element) {};
+        $scope.states = ['event', 'category'];
+        $scope.isCollapsed = true;
+        $scope.currentState = $scope.states[0];
+        //        $scope.activity = [];
 
-            $scope.max = 5;
-            $scope.isReadonly = true;
+        $scope.max = 5;
+        $scope.isReadonly = true;
 
-            // Define resource 
-            //defines the url name
-            //        var actiRes = Restangular.all('activities');
-            //
-            //        // CALL GET
-            //        actiRes.getList().then(function (actiList) {
-            //            angular.forEach(actiList, function (value, key) {
-            //                $scope.activity.push(value);
-            //            });
-            //        });
-            $scope.myInterval = 5000;
-            $scope.noWrapSlides = false;
-            var slides = $scope.slides = [];
-            $scope.addSlide = function () {
-                var newWidth = 600 + slides.length + 1;
-                slides.push({
-                    image: '//placekitten.com/' + newWidth + '/300',
-                    text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' + ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-                });
-            };
-            for (var i = 0; i < 4; i++) {
-                $scope.addSlide();
-            }
-            $scope.change1 = function () {
-                $state.go('app.activity.edit');
-                /*if ($scope.currentState == $scope.states[1]) {
+        // Define resource 
+        //defines the url name
+        //        var actiRes = Restangular.all('activities');
+        //
+        //        // CALL GET
+        //        actiRes.getList().then(function (actiList) {
+        //            angular.forEach(actiList, function (value, key) {
+        //                $scope.activity.push(value);
+        //            });
+        //        });
+        $scope.myInterval = 5000;
+        $scope.noWrapSlides = false;
+        var slides = $scope.slides = [];
+        $scope.addSlide = function () {
+            var newWidth = 600 + slides.length + 1;
+            slides.push({
+                image: '//placekitten.com/' + newWidth + '/300',
+                text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' + ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+            });
+        };
+        for (var i = 0; i < 4; i++) {
+            $scope.addSlide();
+        }
+        $scope.change1 = function () {
+            $state.go('app.activity.edit');
+            /*if ($scope.currentState == $scope.states[1]) {
         $scope.currentState = $scope.states[0];
       } else {
         $scope.currentState = $scope.states[1];
       }*/
-            }
-  }
-])
-    .controller('PplCardController', ['$scope', 'Restangular', '$attrs', '$state',
-  function ($scope, Restangular, $attrs, $state) {
-            this.init = function (element) {};
-            $scope.states = ['event', 'category'];
-            $scope.currentState = $scope.states[0];
-            //        $scope.activity = [];
-
-            $scope.max = 5;
-            $scope.isReadonly = true;
-
-            // Define resource 
-            //defines the url name
-            //        var actiRes = Restangular.all('activities');
-            //
-            //        // CALL GET
-            //        actiRes.getList().then(function (actiList) {
-            //            angular.forEach(actiList, function (value, key) {
-            //                $scope.activity.push(value);
-            //            });
-            //        });
-            $scope.myInterval = 5000;
-            $scope.noWrapSlides = false;
-            var slides = $scope.slides = [];
-            $scope.addSlide = function () {
-                var newWidth = 600 + slides.length + 1;
-                slides.push({
-                    image: '//placekitten.com/' + newWidth + '/300',
-                    text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' + ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-                });
-            };
-            for (var i = 0; i < 4; i++) {
-                $scope.addSlide();
-            }
-            $scope.change1 = function () {
-                $state.go('app.activity.edit');
-                /*if ($scope.currentState == $scope.states[1]) {
-        $scope.currentState = $scope.states[0];
-      } else {
-        $scope.currentState = $scope.states[1];
-      }*/
-            }
-  }
-])
-    .controller('CardController', ['$scope', 'Restangular', '$attrs', '$state',
-  function ($scope, Restangular, $attrs, $state) {
-            this.init = function (element) {};
-            $scope.states = ['event', 'category'];
-            $scope.isCollapsed = true;
-            $scope.currentState = $scope.states[0];
-            //        $scope.activity = [];
-
-            $scope.max = 5;
-            $scope.isReadonly = true;
-
-            // Define resource 
-            //defines the url name
-            //        var actiRes = Restangular.all('activities');
-            //
-            //        // CALL GET
-            //        actiRes.getList().then(function (actiList) {
-            //            angular.forEach(actiList, function (value, key) {
-            //                $scope.activity.push(value);
-            //            });
-            //        });
-            $scope.myInterval = 5000;
-            $scope.noWrapSlides = false;
-            var slides = $scope.slides = [];
-            $scope.addSlide = function () {
-                var newWidth = 600 + slides.length + 1;
-                slides.push({
-                    image: '//placekitten.com/' + newWidth + '/300',
-                    text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' + ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-                });
-            };
-            for (var i = 0; i < 4; i++) {
-                $scope.addSlide();
-            }
-            $scope.change1 = function () {
-                $state.go('app.activity.edit');
-                /*if ($scope.currentState == $scope.states[1]) {
-        $scope.currentState = $scope.states[0];
-      } else {
-        $scope.currentState = $scope.states[1];
-      }*/
-            }
+        }
   }
 ]);
 angular.module('directives')
@@ -245,7 +160,7 @@ angular.module('directives')
             return {
                 restrict: 'EA',
                 templateUrl: '/directive/preferences/layout/paceCard.html',
-                controller: 'PaceCardController',
+                controller: 'CardController',
                 scope: true,
                 link: function (scope, element, attrs, pacecardController) {
                     pacecardController.init(element);
@@ -257,7 +172,7 @@ angular.module('directives')
             return {
                 restrict: 'EA',
                 templateUrl: '/directive/preferences/layout/pplCard.html',
-                controller: 'PplCardController',
+                controller: 'CardController',
                 scope: true,
                 link: function (scope, element, attrs, pplcardController) {
                     pplcardController.init(element);
